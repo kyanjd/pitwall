6fdb180bbe4c6d04b9519ff7410d9f9366d6c541
kyanjd <kyanjd@gmail.com>
2026-01-18 19:24:39 +0000

    Session dep

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -1,1 +1,11 @@
+from typing import Annotated
+
 from app.db.session import get_session_local as get_session
+from fastapi.params import Depends
+from sqlmodel import Session
+
+CurrentSession = Annotated[Session, Depends(get_session)]
+
+
+def get_current_user():
+    pass
