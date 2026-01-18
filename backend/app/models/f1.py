b70543af30585f8364f0f598a85ee37bc29241c5
kyanjd <kyanjd@gmail.com>
2026-01-18 23:47:59 +0000

    F1 tables

---
+++ backend/app/models/f1.py
@@ -0,0 +1,33 @@
+import uuid
+
+from sqlmodel import Field, SQLModel, UniqueConstraint
+
+
+class Race(SQLModel, table=True):
+    __table_args__ = (UniqueConstraint("season", "round"),)
+    id: int = Field(default_factory=uuid.uuid4, primary_key=True)
+    name: str = Field(nullable=False)
+    circuitId: str = Field(foreign_key="circuit.id", nullable=False)
+    round: int = Field(nullable=False, index=True)
+    season: int = Field(nullable=False, index=True)
+    date: str = Field(nullable=False)
+
+
+class Circuit(SQLModel, table=True):
+    id: str = Field(primary_key=True)
+    name: str = Field(nullable=False)
+    country: str = Field(nullable=False)
+
+
+class Result(SQLModel, table=True):
+    id: int = Field(default_factory=uuid.uuid4, primary_key=True)
+    raceId: str = Field(foreign_key="race.id", nullable=False)
+    driverId: str = Field(foreign_key="driver.id", nullable=False)
+    position: int = Field(nullable=False)
+    status: str = Field(nullable=False)
+
+
+class Driver(SQLModel, table=True):
+    id: str = Field(primary_key=True)
+    firstName: str = Field(nullable=False)
+    lastName: str = Field(nullable=False)
