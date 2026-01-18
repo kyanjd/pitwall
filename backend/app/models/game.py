e3e0687ab09e629b36ac1d5ea8a0f502d4346dd7
kyanjd <kyanjd@gmail.com>
2026-01-18 14:27:23 +0000

    Removing circular imports

--- backend/app/models/game.py
+++ backend/app/models/game.py
@@ -3,7 +3,6 @@ from datetime import datetime, timezone
 from sqlmodel import Field, Relationship, SQLModel
 
 from app.models.game_user import GameUser
-from app.models.user import User
 
 
 class Game(SQLModel, table=True):
@@ -11,4 +10,4 @@ class Game(SQLModel, table=True):
     name: str = Field(index=True)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
-    users: list[User] = Relationship(back_populates="games", link_model=GameUser)
+    users: list["User"] = Relationship(back_populates="games", link_model=GameUser)
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,6 +1,5 @@
 from sqlmodel import Field, Relationship, SQLModel
 
-from app.models.game import Game
 from app.models.game_user import GameUser
 
 
@@ -9,4 +8,4 @@ class User(SQLModel, table=True):
     name: str = Field(index=True)
     email: str = Field(unique=True, index=True, nullable=False)
 
-    games: list[Game] = Relationship(back_populates="users", link_model=GameUser)
+    games: list["Game"] = Relationship(back_populates="users", link_model=GameUser)
