8c84130e92bdc8237e8257dd67b313c61606a3f7
kyanjd <kyanjd@gmail.com>
2026-01-18 14:24:24 +0000

    Changes to table with alembic

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,7 +1,13 @@
+import os
 from logging.config import fileConfig
 
 from alembic import context
+from app.models.game import Game
+from app.models.game_user import GameUser
+from app.models.user import User
+from dotenv import load_dotenv
 from sqlalchemy import engine_from_config, pool
+from sqlmodel import SQLModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -16,12 +22,20 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+
+def get_url() -> str:
+    load_dotenv()
+    url = os.getenv("DATABASE_URL")
+    if not url:
+        raise ValueError("DATABASE_URL environment variable is not set")
+    return url
 
 
 def run_migrations_offline() -> None:
@@ -36,7 +50,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -55,8 +69,11 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section)
+    assert configuration is not None
+    configuration["sqlalchemy.url"] = get_url()
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
