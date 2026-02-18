963e97466b5d487e19fa89c05c8e42b20c4f3291
kyanjd <kyanjd@gmail.com>
2026-02-18 22:41:12 +0000

    Current user by decoding token

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -1,11 +1,38 @@
 from typing import Annotated
 
+import jwt
+from app import crud
+from app.core.config import settings
+from app.core.errors import UnauthorizedError
+from app.core.security import ALGORITHM
 from app.db.session import get_session_local as get_session
-from fastapi.params import Depends
+from app.models.user import User
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
 from sqlmodel import Session
 
 CurrentSession = Annotated[Session, Depends(get_session)]
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
-def get_current_user():
-    pass
+
+def get_current_user(
+    session: CurrentSession,
+    token: Annotated[str, Depends(oauth2_scheme)],
+) -> User:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str | None = payload.get("sub")
+        if user_id is None:
+            raise UnauthorizedError("Invalid token.")
+    except InvalidTokenError:
+        raise UnauthorizedError("Invalid token.")
+
+    user = crud.user.get_user_by_id(session=session, user_id=int(user_id))
+    if not user:
+        raise UnauthorizedError("User not found.")
+    return user
+
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
