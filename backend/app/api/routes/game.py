bae168d361dd3f4e18d9b9d3ca6e6a845a67cba5
kyanjd <kyanjd@gmail.com>
2026-02-22 13:28:28 +0000

    More get endpoints for the frontend later

--- backend/app/api/routes/game.py
+++ backend/app/api/routes/game.py
@@ -1,9 +1,8 @@
-from typing import Any
+import uuid
 
 from app import crud
 from app.api.dependencies import CurrentSession, CurrentUser
-from app.core.errors import AlreadyExistsError
-from app.models.game import GameCreate, GameJoin, GamePublic
+from app.models.game import GameCreate, GameJoin, GamePublic, GamePublicWithMembers
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/game", tags=["game"])
@@ -21,6 +20,21 @@ def create_game(session: CurrentSession, current_user: CurrentUser, game_create:
     )
 
 
+@router.get("/", response_model=list[GamePublic])
+def get_games(session: CurrentSession, current_user: CurrentUser) -> list[GamePublic]:
+    games = crud.game.get_games_by_user(session=session, user_id=current_user.id)
+    return [
+        GamePublic(
+            id=game.id,
+            name=game.name,
+            created_at=game.created_at,
+            created_by=game.created_by,
+            invite_code=game.invite_code,
+        )
+        for game in games
+    ]
+
+
 @router.post("/join", response_model=GamePublic)
 def join_game(session: CurrentSession, current_user: CurrentUser, join: GameJoin) -> GamePublic:
     game = crud.game.join_game(session=session, invite_code=join.invite_code, user_id=current_user.id)
@@ -30,4 +44,29 @@ def join_game(session: CurrentSession, current_user: CurrentUser, join: GameJoin
         created_at=game.created_at,
         created_by=game.created_by,
         invite_code=game.invite_code,
+    )
+
+
+@router.get("/{game_id}", response_model=GamePublic)
+def get_game(session: CurrentSession, current_user: CurrentUser, game_id: uuid.UUID) -> GamePublic:
+    game = crud.game.get_game_by_id(session=session, game_id=game_id, user_id=current_user.id)
+    return GamePublic(
+        id=game.id,
+        name=game.name,
+        created_at=game.created_at,
+        created_by=game.created_by,
+        invite_code=game.invite_code,
+    )
+
+
+@router.get("/{game_id}/members", response_model=GamePublicWithMembers)
+def get_game_members(session: CurrentSession, current_user: CurrentUser, game_id: uuid.UUID) -> GamePublicWithMembers:
+    game, members = crud.game.get_game_by_id_with_members(session=session, game_id=game_id, user_id=current_user.id)
+    return GamePublicWithMembers(
+        id=game.id,
+        name=game.name,
+        created_at=game.created_at,
+        created_by=game.created_by,
+        invite_code=game.invite_code,
+        members=[member.id for member in members],
     )
